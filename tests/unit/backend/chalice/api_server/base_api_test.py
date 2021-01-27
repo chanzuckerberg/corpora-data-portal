94ae40da62bedbc7a98cb99f26593825faf3cd3a
Michael Barrientos <mbarrientos@chanzuckerberg.com>
2021-01-26 23:07:56 -0800

    Replace Mock OAuth with in-code mock

--- tests/unit/backend/chalice/api_server/base_api_test.py
+++ tests/unit/backend/chalice/api_server/base_api_test.py
@@ -3,7 +3,6 @@ import sys
 
 from tests.unit.backend.chalice import ChaliceTestHarness
 from tests.unit.backend.corpora.fixtures.environment_setup import EnvironmentSetup
-from tests.unit.backend.chalice.api_server.mock_auth import MockOauthServer
 from tests.unit.backend.fixtures.data_portal_test_case import DataPortalTestCase
 
 
@@ -52,22 +51,17 @@ class BaseAuthAPITest(BaseAPITest):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.mock_oauth_server = MockOauthServer()
-        cls.mock_oauth_server.start()
-        assert cls.mock_oauth_server.server_okay
-
         cls.old_path = sys.path.copy()
         sys.path.insert(0, os.path.join(cls.corpora_api_dir, "chalicelib"))  # noqa
         from corpora.common.corpora_config import CorporaAuthConfig
 
         # Use the CorporaAuthConfig used by the chalice app
         cls.auth_config = CorporaAuthConfig()
-        cls.auth_config._config["api_base_url"] = f"http://localhost:{cls.mock_oauth_server.port}"
+        cls.auth_config._config["api_base_url"] = ""
         cls.auth_config._config["callback_base_url"] = "http://localhost:5000"
         cls.auth_config.update_defaults()
 
     @classmethod
     def tearDownClass(cls):
         super().tearDownClass()
-        cls.mock_oauth_server.terminate()
         sys.path = cls.old_path
